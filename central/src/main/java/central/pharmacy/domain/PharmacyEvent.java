61d52f478310e258897d82b6bdcafea677d46614
“markvickers-imp” <mark.vickers@improving.com>
2025-08-07 09:27:13 -0400

    added pharmacy event crud

---
+++ central/src/main/java/central/pharmacy/domain/PharmacyEvent.java
@@ -0,0 +1,16 @@
+package central.pharmacy.domain;
+
+import akka.javasdk.annotations.TypeName;
+
+public interface PharmacyEvent {
+
+    @TypeName("pharmacy-created")
+    record PharmacyCreated(Pharmacy pharmacy) implements PharmacyEvent {}
+
+    @TypeName("pharmacy-updated")
+    record PharmacyUpdated(Pharmacy pharmacy) implements PharmacyEvent {}
+
+    @TypeName("pharmacy-deleted")
+    record PharmacyDeleted() implements PharmacyEvent {}
+
+}
