358956b30a46478c2c7729330fbb559ffa727371
Mark Vickers <markjvickers@hotmail.com>
2025-08-11 11:45:42 -0400

    zed refinements

--- store/src/main/java/pharmacy/Bootstrap.java
+++ store/src/main/java/pharmacy/Bootstrap.java
@@ -5,8 +5,6 @@ import akka.javasdk.ServiceSetup;
 import akka.javasdk.annotations.Setup;
 import akka.javasdk.http.HttpClientProvider;
 import com.typesafe.config.Config;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import pharmacy.application.central.client.CentralClient;
 import pharmacy.application.central.client.CentralClientImpl;
 import pharmacy.domain.PharmacyId;
@@ -14,7 +12,6 @@ import pharmacy.domain.PharmacyId;
 @Setup
 public class Bootstrap implements ServiceSetup {
 
-    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final HttpClientProvider httpClientProvider;
     private final Config config;
 
@@ -30,10 +27,15 @@ public class Bootstrap implements ServiceSetup {
             @Override
             public <T> T getDependency(Class<T> clazz) {
                 if (clazz == CentralClient.class) {
-                  return (T) new CentralClientImpl(config, httpClientProvider);
+                    return (T) new CentralClientImpl(
+                        config,
+                        httpClientProvider
+                    );
                 }
                 if (clazz == PharmacyId.class) {
-                    return (T) new PharmacyId(config.getString("store.pharmacy-id"));
+                    return (T) new PharmacyId(
+                        config.getString("store.pharmacy-id")
+                    );
                 }
                 return null;
             }
